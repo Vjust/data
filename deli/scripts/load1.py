9e754d925fe395b13385ad8a3f03bb473feaf865
Vijay Rao <vjust@kreya1.(none)>
2012-10-06 17:03:09 +0000

    changes

--- deli/scripts/load1.py
+++ deli/scripts/load1.py
@@ -18,34 +18,19 @@
 # u'updated': u'Fri, 11 Sep 2009 15:07:02 +0000',
 # u'wfw_commentrss': u'http://feeds.delicious.com/v2/rss/url/90c6b38a375cf62280f9ebc89823f570'}
 #
-#--
-#--create table links (
-#--	id serial primary key,
-#--	author varchar(40),
-#--	link_id varchar(200),
-#--	url varchar(200),
-#--	type varchar(40),
-#--	tags varchar(100),
-#--	title varchar(200),
-#--	value varchar(100),
-#--	updated date);
-#
-
-
+ 
 import json
 import psycopg2
 
-file=open("/home/vjust/data/inputs/testfile")
+file=open("/home/vjust/data/deli/inputs/testfile")
 lines=file.readlines()
 file.close()
-
-
 
 conn=psycopg2.connect("dbname=deli user=vjust")
 conn.autocommit=True
 cur=conn.cursor()
 
-for ll in lines[:10]:
+for ll in lines:
 	obj=json.loads(ll)
 	author=obj['author']
 	link_id=obj['id']
@@ -56,6 +41,8 @@ for ll in lines[:10]:
 	#value=obj['value']
 	updated=obj['updated']
 	print "author:%s\nlink_id:%s\nurl:%s\ntags:%s\ntitle:%s\nupdated:%s\n" % (author, link_id, url, tags, title,  updated)
-    #cur.execute("insert into links (author, link_id, url, tags, title) values 
+	cur.execute("insert into links (author, link_id, url, tags, title) values (%s , %s, %s, %s, %s)", (author, link_id, url, str(tags), title))
+
+cur.close()
 conn.close()
 conn.notices
